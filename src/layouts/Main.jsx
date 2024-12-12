c1c85fda90ae14267134e9ec37dbbd08c400c2b9
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-12-12 16:49:23 +0100

    ADD flags

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,20 +1,36 @@
 import { useContext } from "react"
 import GlobalContext from "../contexts/GlobalContext"
+import IT from "../assets/it.webp"
+import ES from "../assets/es.webp"
+import EN from "../assets/us.webp"
+import FR from "../assets/fr.webp"
 
 export default function Main() {
 
     const { films } = useContext(GlobalContext)
 
+    const flagImages = {
+        it: IT,
+        en: EN,
+        fr: FR,
+        es: ES
+    };
+
+
     return (
         <main>
+
             <ul>
                 {
                     films.map(film => {
+
+                        const flagImage = flagImages[film.original_language]
+
                         return <li key={film.id}>
                             <div>{film.name}</div>
                             <div>{film.original_name}</div>
-                            <div>{film.original_language}</div>
                             <div>{film.vote_average}</div>
+                            {flagImage && <img src={flagImage} alt="" />}
                         </li>
                     })
                 }
