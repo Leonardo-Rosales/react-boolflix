92254fca184e7e70b6c75dd1bd16c93f6f8199c3
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-12-13 14:03:59 +0100

    ADD milestone 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
-import { useContext } from "react"
-import GlobalContext from "../contexts/GlobalContext"
 import IT from "../assets/it.webp"
 import ES from "../assets/es.webp"
 import EN from "../assets/us.webp"
 import FR from "../assets/fr.webp"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons"
 
 
 export default function Card({ item }) {
@@ -18,10 +19,27 @@ export default function Card({ item }) {
         es: ES
     }
 
+    let vote = Math.ceil(vote_average / 2)
+    // console.log(vote)
+    const stars = []
+
+    for (let i = 0; i < 5; i++) {
+
+        if (i < vote) {
+            stars.push(faStarSolid)
+        } else {
+            stars.push(faStarRegular)
+        }
+    }
+
 
     return (
         <div>
-            <img src={`${IMG_URI}${poster_path}`} alt="" />
+
+            <figure>
+                <img src={`${IMG_URI}${poster_path}`} alt="" />
+            </figure>
+
             <h3>{title || name}</h3>
             <p>{original_title || original_name}</p>
 
@@ -30,7 +48,13 @@ export default function Card({ item }) {
                     <img src={flagImages[original_language]} alt="" /> :
                     <p>{original_language}</p>
             }
-            <p>{vote_average}</p>
+
+            <div>
+                {stars.map((star, i) => (
+                    <FontAwesomeIcon key={i} icon={star} />
+                ))}
+            </div>
+
         </div>
     )
 }
